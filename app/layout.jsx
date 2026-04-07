a9dc125f7fda4f4a3a49479cd82e2af939e66d67
Aysha Kassem <Ayshakassem59@gmail.com>
2026-04-07 04:09:35 +0200

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="format-detection" content="telephone=no,email=no,address=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="stylesheet"
